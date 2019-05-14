3dd4dcecf286e15eb41feeee4b7b580bdc372c80
Никита Молодцов <nikita.molodtsov21@gmail.com>
2019-05-15 00:25:34 +0300

    Finished dict builder and compressor

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 class archive {
 private:
-	static int counter;
+	int counter;
 	string debug;
 	string input;
 	string output;
@@ -26,51 +26,26 @@ archive::archive(const string &input, const string &output) {
 	counter = 0;
 }
 void archive::BuildDict() {
+	counter = 0;
 	cout << "\n\n========= Filling the first dictionary =========\n" << endl;
-	ifstream stream("C:\\a\\w.txt", ios::binary);
-
-	for (const auto& x : debug) {
-		string temp;
+	ifstream stream(input, ios::binary);
+	string temp;
+	char x;
+	while (stream) {
+		stream.get(x);
 		temp.push_back(x);
 		if (dict_original.find(temp) != dict_original.end()) continue;
 		dict_original[temp] = ++counter;
 		cout << temp << " " << dict_original[temp] << endl;
+		temp.pop_back();
 	}
+	stream.close();
+	cout << "\n\n========= Filled the first dictionary =========\n" << endl;
 }
 void archive::Compress() {
-
 }
 
 
-int main() {
-	map<string, int> dict = dict_original;
-	string temp_s;
-	char temp_c;
-	string encoded = to_string(dict_original.size());
-
-	cout << "\n\n========= Encoding the file =========\n" << endl;
-	for (const auto &item : dict_original) {
-		cout << item.first << " : " << item.second << endl;
-		encoded += item.first;
-	}
-	for (rsize_t i = 0; i < file.length(); i++) {
-		temp_c = file[i];
-		if (dict.find(temp_s + to_string(temp_c)) != dict.end()) {
-			temp_s += to_string(temp_c);
-		}
-		else {
-			encoded += to_string(dict[temp_s]);
-			encoded += ',';
-			dict[temp_s + to_string(temp_c)] = ++counter;
-			temp_s = to_string(temp_c);
-		}
-	}
-	cout << "Your code: " << encoded << endl;
-	cout << "File size = " << file.length() << "; Code size = " << encoded.length() - file.length() << endl;
-
-	system("pause");
-	return 0;
-}
 void test() {
 	string file = "eejhbasvkjndfvkjncvm,ncv,nm,vmcnvb,mnzvcv;sdjjrkvbcnvmxawehjbjhbskdvjbdskvjlxdjfsef";
 	cout << "File: " << file << endl;
@@ -108,4 +83,10 @@ void test() {
 	}
 	cout << "Your code: " << encoded << endl;
 	cout << "File size = " << file.length() << "; Code size = " << encoded.length() - file.length() << endl;
+}
+int main() {
+	archive Archive("C:\\a\\w.txt", "C:\\a\\out.zipped");
+	Archive.BuildDict();
+	system("pause");
+	return 0;
 }
