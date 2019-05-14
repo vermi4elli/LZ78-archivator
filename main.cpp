f6691e1588c8cf3b6232dc3718178270500b3d31
Никита Молодцов <nikita.molodtsov21@gmail.com>
2019-05-15 00:43:56 +0300

    fixed last commit

--- main.cpp
+++ main.cpp
@@ -7,86 +7,133 @@ using namespace std;
 
 class archive {
 private:
+	int size;
 	int counter;
 	string debug;
 	string input;
 	string output;
 	map<string, int> dict_original;
 public:
-	archive();
-	archive(const string &input, const string &output);
+	archive(const string& input, const string& output);
 	void BuildDict();
 	void Compress();
 };
 
-archive::archive() {}
-archive::archive(const string &input, const string &output) {
+archive::archive(const string& input, const string& output) {
 	this->input = input;
 	this->output = output;
 	counter = 0;
+	size = 0;
 }
 void archive::BuildDict() {
-	counter = 0;
-	cout << "\n\n========= Filling the first dictionary =========\n" << endl;
+	//cout << "\n\nFilling the dictionary...\n";
+
+	//Создал входящий поток
 	ifstream stream(input, ios::binary);
-	string temp;
+
+	//Создал переменную строку и проинициализировал 1 символом
+	//т.к. в цикле while ниже я первым делом вызываю pop_back() у строки
+	string temp = ".";
+
+	//просто символ, который будет таскать символы из файла
 	char x;
+
 	while (stream) {
+		//очищаю строку
+		temp.pop_back();
+
+		//получаю символ из файла и пушу его в строку
 		stream.get(x);
 		temp.push_back(x);
+
+		//проверяю словарь на наличие совпадений
+		//если есть, то следующая итерация
 		if (dict_original.find(temp) != dict_original.end()) continue;
+
+		//иначе создаю пару в словаре и вывожу на экран(при отладке)
 		dict_original[temp] = ++counter;
-		cout << temp << " " << dict_original[temp] << endl;
-		temp.pop_back();
+		//cout << temp << " " << dict_original[temp] << endl;
 	}
+
+	//закрываю файл
 	stream.close();
-	cout << "\n\n========= Filled the first dictionary =========\n" << endl;
+
+	//запоминаю размер "оригинального" словаря(односимвольных уникальных фраз)
+	size = dict_original.size();
+
+	//cout << "Filled the dictionary!\n" << endl;
 }
 void archive::Compress() {
-}
+	cout << "\n\nCompressing file " << input << "...\n";
 
+	//Создаю два файловых потока
+	ifstream in(input, ios::binary);
+	ofstream out(output);
 
-void test() {
-	string file = "eejhbasvkjndfvkjncvm,ncv,nm,vmcnvb,mnzvcv;sdjjrkvbcnvmxawehjbjhbskdvjbdskvjlxdjfsef";
-	cout << "File: " << file << endl;
-	int counter = 0;
-	map<string, int> dict_original;
-	cout << "\n\n========= Filling the first dictionary =========\n" << endl;
-	for (const auto& x : file) {
-		string temp;
-		temp.push_back(x);
-		if (dict_original.find(temp) != dict_original.end()) continue;
-		dict_original[temp] = ++counter;
-		cout << temp << " " << dict_original[temp] << endl;
+	//Так мы записываем кол-во элементов в словаре
+	//(или сколько нужно итераций цикла для алгоритма декомпресса)
+	out.write((char*)& size, sizeof(size));
+	//out << size;
+
+	//После записи кол-ва элементов, записываем все элементы словаря
+	//(так, как используем std::map, а это r&b tree, то они будут по порядку)
+	//Таким образом, при декомпрессинге, мы сможем заполнить заново "односимвольный" словарь
+	// просто прочитав ту часть файла, которую запишем
+	for (const auto& item : dict_original) {
+		out.write((char*)& item.first, sizeof(item.first));
+		//out << item.first;
 	}
+
+	//Создал второй словарь, который уже сможет хранить многосимвольные фразы
 	map<string, int> dict = dict_original;
-	string temp_s;
-	char temp_c;
-	string encoded = to_string(dict_original.size());
 
-	cout << "\n\n========= Encoding the file =========\n" << endl;
-	for (const auto &item : dict_original) {
-		cout << item.first << " : " << item.second << endl;
-		encoded += item.first;
-	}
-	for (rsize_t i = 0; i < file.length(); i++) {
-		temp_c = file[i];
+	//Создал переменную строку(буфер) и указатель на чары(ниже в алгоритме пояснено)
+	string temp_s;
+	char* temp;
+
+	//просто символ, который будет таскать символы из файла
+	char temp_c;
+
+	//Теперь алгоритм компресса самого файла
+	while (in) {
+		//Получаю из файла символ
+		in.get(temp_c);
+
+		//Если есть совпадение буфер+символ в словаре, прибавляю его в буфер и к следующей итерации
 		if (dict.find(temp_s + to_string(temp_c)) != dict.end()) {
 			temp_s += to_string(temp_c);
 		}
+		//Если же нет совпадения, то
 		else {
-			encoded += to_string(dict[temp_s]);
-			encoded += ',';
+			//cout << "\n\nbegin\n\n" << temp_s << "\n\n" << temp_s.c_str() << "\n\nend\n\n";
+			//Записываю в файл тот буфер, который имеется на данный момент
+			int temp_t = dict[temp_s];
+			out.write((char*)& temp_t, sizeof(temp_t));
+			//out << temp_t;
+
+			//Делаю новую запись в словаре с данной строкой
 			dict[temp_s + to_string(temp_c)] = ++counter;
+
+			//Буфер очищается и приравнивается к текущему значению последнего символа
 			temp_s = to_string(temp_c);
 		}
 	}
-	cout << "Your code: " << encoded << endl;
-	cout << "File size = " << file.length() << "; Code size = " << encoded.length() - file.length() << endl;
+
+	//закрываю файлы
+	in.close();
+	out.close();
+
+	//Обнуляю счетчик для возможного декомпрессинга
+	counter = 0;
+
+	cout << "Compressed file " << input << "!" << endl;
 }
+
 int main() {
-	archive Archive("C:\\a\\w.txt", "C:\\a\\out.zipped");
+	string input = "C:\\a\\w.txt";
+	archive Archive(input, input + ".lzw");
 	Archive.BuildDict();
+	Archive.Compress();
 	system("pause");
 	return 0;
 }
