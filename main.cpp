a7b2608c2a48dae3cc7e7f24b621bdca618b0e48
Никита Молодцов <42549298+vermi4elli@users.noreply.github.com>
2019-05-18 04:31:44 +0300

    всэ

--- main.cpp
+++ main.cpp
@@ -16,13 +16,14 @@ typedef map<string, int>  Map; // упрощение, чтобы не писат
 class archive
 {
 	string buffer; // буфер для хранения
+	int pos; // позиция в словаре
 
 	ofstream output; // выходной файл
 	ifstream input; //входной файл
 public:
 	void compress(char const**, int); //сжатие
-	string compress1(string);// сжатие 78
-	int len(int); //та же функция для вычисления длины "бинарной строки"
+	string compress1(string);// сжатие 77
+	int len(int);  //та же функция для вычисления длины "бинарной строки"
 	string to_binary_string(unsigned long int, int); // перевод в бинарное значение
 	string write_bits(string);
 	void decompress(string); //деархивация
@@ -31,7 +32,6 @@ public:
 	void write_decompress(string&); //декомпрессия
 	void cache_in();//получаение размера файла
 	~archive();
-	archive() {};
 };
 archive::~archive()
 {
@@ -44,11 +44,10 @@ int archive::bits_to_int(string bits)
 {
 	//инициализация переменных
 	int result = 0, i = 0, size = bits.size();
-	
+
 	//смещение на первый ненулевой бит
 	while (bits[i] == 0)
 		i++;
-
 	for (; i < size; i++)
 	{
 		result += (int(bits[i]) - int('0')) * int(pow(2, size - i - 1));
@@ -58,8 +57,6 @@ int archive::bits_to_int(string bits)
 //получаем биты. Используем при деархивации
 string archive::get_bits(int lenght)
 {
-	// if (buffer.empty())
-	//     return "";
 	string result = buffer.substr(0, lenght);
 	buffer.erase(0, lenght);
 	return result;
@@ -87,13 +84,11 @@ void archive::decompress(string path)
 	//инициализация переменных
 	string buff = "", newItem;
 	string result = "";
+	int i = 0, k = 0;
 
 	//Создаем первое поле в векторе стрингОв "словарик" - пустая строка
 	vector<string> dictionary;
 	dictionary.push_back(buff);
-
-	//опять инициализация переменных
-	int i = 0, k = 0;
 
 	//получаем первый бит
 	buff = get_bits(1);
@@ -129,11 +124,12 @@ void archive::decompress(string path)
 
 void archive::write_decompress(string & bitstring)
 {
+	ofstream out;
 	//имя файла
 	string filename = "";
-	
+
 	//размер файла
-	int size_of_file; 
+	int size_of_file;
 	int i = 0, limit = bitstring.size() - bitstring.size() % 8;
 	char c;
 
@@ -149,10 +145,8 @@ void archive::write_decompress(string & bitstring)
 			c = bits_in_byte(bitstring.substr(i, BITS_PER_BYTE)).to_ulong();
 			i += 8;
 		}
-		cout << "Getting out file " << filename << endl;
-		ofstream out(filename, ios::binary);
-		if (!out.is_open()) cout << "... Done!" << endl;
-		else cout << "... Error!" << endl;
+		cout << "Getting out file " << filename << "... Done!" <<endl;
+		out.open(filename, ios::binary);
 
 		//Сколько нужно записывать
 		size_of_file = bits_to_int(bitstring.substr(i, 64));
@@ -185,65 +179,58 @@ void archive::compress(char const** argv, int argc)
 	//открываем файл и записываем его содержимое в виде битов в строку для хранения
 	for (int i = 2; i < argc - 1; i++)
 	{
-		cout << "Compressing file " << argv[i] << "... ";
+		cout << "Compressing file " << argv[i] << "... Done."; 
 		input.open(argv[i], ios::binary);
-		if (!input.is_open()) {
-			cout << "Error!" << endl;
-			continue;
-		}
 		bitstring += write_bits(argv[i]);
 		input.close();
-		cout << "Done!" << endl;
 	}
 	bitstring = compress1(bitstring); // архивируем содержимое
 	bitstring.erase(0, 1); //перепроверяем-с bitstring на чистоту
-	
 	//Завершаем незавершенный байт, если таковой имеется
 	while (bitstring.size() % BITS_PER_BYTE)
 	{
-		cout << "0";
 		bitstring += '0';
 	}
 	cout << endl;
-
 	//записывам результат в новый файл
 	output.open(argv[argc - 1], ios::binary);
-	for (size_t i = 0; i < bitstring.size(); i += BITS_PER_BYTE)
+	for (std::size_t i = 0; i < bitstring.size(); i += BITS_PER_BYTE)
 	{
 		byte b = bits_in_byte(bitstring.substr(i, BITS_PER_BYTE)).to_ulong();
 		output << b;
 	}
 }
-
 //получаем биты для файла и его имени
 string archive::write_bits(string filename)
 {
 	string buff = ""; // для хранения содержимого
 	string bitstring = ""; // для хранения имени файла, а после и всей последовательности байтов в файле
 	char c;
+	cout << "filename: " << filename << " ..." << endl; //выводим имя файла
 	for (size_t i = 0; i < filename.size(); i++) //проходимся по размеру имени файла
 	{
 		bitstring += bits_in_byte(byte(filename.at(i))).to_string(); //переводим биты в байты
 	}
 	bitstring += bits_in_byte(byte('\a')).to_string();//добавляем после названия файла битовое значение ЗВУКОВОГО СИГНАЛА для того, чтобы отделить название файла и его содержимое. При архивации и деархивации будет звуковой сигнал показывающий что файл успешно открыт
-	
 	while (input.get(c)) {
 		buff += bits_in_byte(byte(c)).to_string(); //переводим биты в байты
 	}
+	cout << "size of file (without name): " << buff.size() << endl; // выводим размер файла (без имени)
 	bitstring += to_binary_string(buff.size(), 64); // переводим биты в байты
 	bitstring.append(buff); // добавляем содержимое файла в байтах в последовательность байтов файла
 	return bitstring;
 }
-//процесс архивации LZ78
+//процесс архивации 78
 string archive::compress1(string bitstring)
 {
+
 	string buff = "";
 	string result = "";
 	Map dictionary = {};// словарь для хранения фраза, позиция в "словарике"
 	dictionary.insert(pair<string, int>("", 0)); //пушим пустую строку ключом 0
 	//ну и цикл, собственно, компрессирования
 	for (int i = 0; i < bitstring.size(); i++) // идем по строке
-	{ 
+	{
 		if (dictionary.find(buff + bitstring.at(i)) != dictionary.end()) // если есть совпадение "временная строка" + "текущий символ" в словаре, то
 		{
 			buff += bitstring.at(i); // добавляем в временную строку комбинацию
@@ -266,7 +253,6 @@ int archive::len(int number)
 {
 	return ceil(log2(number));
 }
-
 //Переводим тот ключик из словаря, и делаем из него бинарный код, запихиваем его в строку, возвращаем строку
 string archive::to_binary_string(unsigned long int n, int lenght)
 {
@@ -288,24 +274,19 @@ string archive::to_binary_string(unsigned long int n, int lenght)
 }
 int main(int argc, char const* argv[])
 {
-	archive rar1; // создаем переменную рар
+	archive win; // создаем переменную win))))
 
-	// получаем параметр
-	string parameter(argv[1]);
-
-	//выводим параметр
-	cout << parameter << endl;
+	string parameter(argv[1]); // получаем параметр
+	cout << parameter << endl;//выводим параметр
 
 	if (parameter == "--decompress") // деархивация
 	{
 		string path_to_archive(argv[2]);
-		rar1.decompress(path_to_archive);
-		cout << argc - 2 << "files written." << endl;
+		win.decompress(path_to_archive);
 	}
 	if (parameter == "--compress") // архивация
 	{
-		rar1.compress(argv, argc);
-		cout << "Result written to " << argv[argc - 1] << endl;
+		win.compress(argv, argc);
 	}
 	return 0;
 }
